476b7531d0048246b4fd343d3330f5a5a90e6073
Thao Chi Vu <tv2145@nyu.edu>
2023-11-12 22:45:31 -0500

    p5play homework + readme

--- README.md
+++ README.md
@@ -1,60 +1,10 @@
-# p5.js Template
-
-This is a README file that can be used to describe and document your assignment.
-
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
-
----
----
-
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
-
-**bold text**
-
-*italicized text*
-
-~~strikethrough text~~
-
-Ordered List:
-1. First item
-2. Second item
-3. Third item
-
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
-
-
-Image with url:  
-![image description](https://dm-gy-6063-2023f-d.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
+# HW09 Description (Sun Set Game)
+1. Which library(ies) did you choose? Include link(s).  
+  https://p5play.org/learn/
+2. During class, you mentioned Flappy Bird, and because I used to obsess over this game in high school, I really wanted to try p5play for myself and see how the effect could be acheived.
+  The concept itself was inspired by last weekend's daylight saving, which is arguably my least favorite part of the year, when the sun starts setting at 4pm. If the sun sets (touches the trees), you lose! Press R to restart the game.
+3. One of the most challenging things was figuring out the abilities of the library and understanding what it's doing. Additionally, although we have learned about them plenty, I have not used objects and constructors in my mid-term, so I felt like I had to dig deeper to make sure they work the way they should in this homework.  
+Additionally, despite wanting to make a curve, that would look like hills or a horizon, I figured it would be much easier to create rectangles (trees), spaced out, via a for loop.
+4. After I somewhat got the mechanics into a satisfactory state, I wanted to make the game visually appealing, starting out with a sketch, then mockup in Figma and then implementing it in the draw function
+![GdomCX.png](https://imgpile.com/images/GdomCX.png)  
+![Gdofea.png](https://imgpile.com/images/Gdofea.png)
--- sketch.js
+++ sketch.js
@@ -1,6 +1,161 @@
+let sun;
+let obstacles = [];
+let gameActive = true;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  sun = new Sun();
 }
 
 function draw() {
+  //background & clourds
+  background(145,237,255);
+  fill(71,208,239,60)
+  rect(-25,0,450,75,200)
+  rect(-25,150,450,75,200)
+
+  fill(71,208,239,90)
+  rect(45,75,450,75,200)
+
+  fill(71,208,239,60)
+  rect(width-425,50,450,75,200)
+  rect(width-425,200,450,75,200)
+
+  fill(71,208,239,90)
+  rect(width-400,125,450,75,200)
+
+
+  if (gameActive) {
+    // create obstacles
+    for (let i = obstacles.length - 1; i >= 0; i--) {
+      obstacles[i].show();
+      obstacles[i].update();
+
+      // Check for collision with sun
+      if (obstacles[i].hits(sun)) {
+        console.log("Game Over. Press 'R' to restart the game.");
+        gameActive = false;
+        noLoop(); // Stop 
+      }
+
+      // off-screen obstacles deleted
+      if (obstacles[i].offscreen()) {
+        obstacles.splice(i, 1);
+      }
+    }
+
+    sun.show();
+    sun.update();
+
+    // Create new obstacles
+    if (frameCount % 75 === 0) {
+      obstacles.push(new Obstacle());
+    }
+  }
+}
+
+//space bar and reset controls
+function keyPressed() {
+  if (key === ' ' && gameActive) {
+    sun.up();
+  } else if ((key === 'R' || key === 'r') && !gameActive) {
+    resetGame();
+  }
+}
+
+function resetGame() {
+  sun = new Sun();
+  obstacles = [];
+  gameActive = true;
+  loop();
+}
+
+function Sun() {
+  this.y = height / 2;
+  this.x = 150;
+  this.gravity = 0.7;
+  this.velocity = 0;
+
+  this.show = function() {
+    //Sun petals
+    fill(255,153,0);
+    ellipse(this.x-75, this.y, 100, 100);
+    ellipse(this.x+75, this.y, 100, 100);
+    ellipse(this.x-40, this.y+65, 100, 100);
+    ellipse(this.x+40, this.y+65, 100, 100);
+    ellipse(this.x-40, this.y-65, 100, 100);
+    ellipse(this.x+40, this.y-65, 100, 100);
+
+    //sun center
+    fill(255,176,0);
+    noStroke()
+    ellipse(this.x, this.y, 160, 160);
+
+    //sun eyes
+    fill("white");
+    ellipse(this.x-35, this.y-10, 60, 60);
+    ellipse(this.x+35, this.y-10, 60, 60);
+    fill("black");
+    ellipse(this.x-20, this.y-10, 30, 30);
+    ellipse(this.x+50, this.y-10, 30, 30);
+
+  };
+
+  this.up = function() {
+    this.velocity += -this.gravity * 30;
+  };
+
+  this.update = function() {
+    this.y += this.velocity;
+    this.velocity += this.gravity;
+
+    // floor & ceiling implementation
+    if (this.y > height - 100) {
+      this.y = height - 100;
+      this.velocity = 0;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
+    }
+  };
+}
+
+function Obstacle() {
+  this.width = random(150, 250);
+  this.height = random(75, height / 2);
+  this.x = width;
+  this.y = height - this.height-30;
+
+  this.show = function() {
+    fill(0,200,103);
+    rect(this.x, this.y, this.width, this.height, 50);
+
+    fill (0, 176, 89)
+    rect(this.x+(this.width-60), this.y+(this.height/8), 40, 50, 50);
+
+    fill (175,47,0)
+    rect(this.x+(this.width/2.5), this.y+this.height, 50, 75, 0);
+
+
+  };
+
+  this.update = function() {
+    this.x -= 5;
+  };
+
+  this.offscreen = function() {
+    return this.x < -this.width;
+  };
+
+  this.hits = function(sun) {
+    // collision detection
+    return (
+      sun.x < this.x + this.width &&
+      sun.x + 120 > this.x &&
+      sun.y < this.y + this.height &&
+      sun.y + 120 > this.y
+    );
+  };
 }
